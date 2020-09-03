3f2132e24e8702bb87d6a1351a573e4cb8b2722f
detectiverust93 <lucas.henrique0023@gmail.com>
2020-09-03 19:38:11 -0300

    Criamos a logica para validar os dados recebidos pelo cliente.

--- src/main/java/br/com/softblue/sbpay/SBPayService.java
+++ src/main/java/br/com/softblue/sbpay/SBPayService.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SBPayService {
+	
+	private static final String AUTH_TOKEN = "r2d2";
 
 	@PostMapping(path = "/pay", consumes = "application/json", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<PaymentResponse> pagar(
@@ -19,7 +21,18 @@ public class SBPayService {
 			@Valid @RequestBody DadosCartao dadosCartao,
 			Errors errors){
 		
+		if (!AUTH_TOKEN.equals(token)) {
+			return ResponseEntity.badRequest().body(new PaymentResponse("Token invalido"));
+		}
 		
+		if (errors.hasErrors()) {
+			return ResponseEntity.ok().body(new PaymentResponse(StatusPagamento.CartaoInvalido));
+		}
+		
+		String numCartao = dadosCartao.getNumCartao();
+		
+		StatusPagamento status = numCartao.startsWith("1111") ? StatusPagamento.Autorizado : StatusPagamento.NaoAutorizado;
+		return ResponseEntity.ok().body(new PaymentResponse(status));
 		
 	}
 }
